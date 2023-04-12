18d925b092e8710d01b1ba57c6ede5ed58fd8449
charbs0701 <chaqhdtjr7@gmail.com>
2023-04-12 23:57:20 +0900

    Feat Take-off Btn

--- wdc/src/components/TakeoffBtn.jsx
+++ wdc/src/components/TakeoffBtn.jsx
@@ -1,9 +1,17 @@
+import React from 'react';
 import Btn from './Btn';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3001'); // WebSocket 서버 주소
+
+const handleTakeoffClick  = () => {
+    socket.emit('takeoff'); // takeoff 이벤트 메시지 전송
+  }
 
 const TakeoffBtn = () => {
-    return (
-        <Btn value="Take-Off"/>
-    );
+  return (
+    <Btn value="Take-Off" onClick={handleTakeoffClick} />
+  );
 };
 
 export default TakeoffBtn;
