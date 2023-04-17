5643269e32d42998c93bc71f70f1651020ed4809
charbs0701 <chaqhdtjr7@gmail.com>
2023-04-18 00:58:50 +0900

    Feat 이륙, 착륙 키보드입력

--- src/components/LandingBtn.jsx
+++ src/components/LandingBtn.jsx
@@ -1,13 +1,22 @@
+import React from "react";
 import Btn from "./Btn";
 import socket from "../socket";
 
-const handleLandingClick = () => {
-    console.log("Landing 클릭");
-    socket.emit("landing"); // landing 이벤트 메시지 전송
+const handleLanding = () => {
+  console.log("Landing 명령 입력");
+  socket.emit("landing"); // landing 이벤트 메시지 전송
 };
 
 const LandingBtn = () => {
-  return <Btn value="Landing" onClick={handleLandingClick} />;
+  return <Btn value="Landing" />;
+};
+
+LandingBtn.onClick = () => handleLanding();
+LandingBtn.onKeyDown = (e) => {
+  // l 버튼 누르면 착륙
+  if (e.key === "l") {
+    handleLanding();
+  }
 };
 
 export default LandingBtn;
--- src/components/TakeoffBtn.jsx
+++ src/components/TakeoffBtn.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Btn from "./Btn";
 import socket from "../socket";
 
-const handleTakeoffClick = () => {
-  console.log("Takeoff 클릭");
+const handleTakeoff = () => {
+  console.log("Takeoff 명령 입력");
   socket.emit("takeoff"); // takeoff 이벤트 메시지 전송
 };
 
 const TakeoffBtn = () => {
-  return <Btn value="Take-Off" onClick={handleTakeoffClick} />;
+  return <Btn value="Take-Off" />;
+};
+
+TakeoffBtn.onClick = () => handleTakeoff();
+TakeoffBtn.onKeyDown = (e) => {
+  // t 버튼 누르면 이륙
+  if (e.key === "t") {
+    handleTakeoff();
+  }
 };
 
 export default TakeoffBtn;
